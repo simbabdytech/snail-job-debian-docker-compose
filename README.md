6491d1b669faf4a41f266d7f2969b3add2d02db2
simbabdytech <simba@bdy.tech>
2024-12-18 18:11:29 +0800

    init

--- README.md
+++ README.md
@@ -32,3 +32,13 @@ $ ./clean-docker.sh
 $ ./test-docker.sh
 ```
 
+
+
+```text
+浏览器打开 http://127.0.0.1:8080/snail-job/
+用户名: admin
+密码: admin
+
+登录后台管理，可以配置群组、 命名空间等
+```
+
--- test-docker.sh
+++ test-docker.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 echo "测试 snail-job 8080"
-curl -I http://127.0.0.1:8080
+curl -I http://127.0.0.1:8080/snail-job
+
+sleep 2
 
 echo "测试 snail-job 1788"
 # 启动telnet并将其放入后台运行
 telnet 127.0.0.1 1788 &
+
+
+sleep 2
 
 echo "测试 mysql 3306"
 mysql -h 127.0.0.1 -P 3306 -u pig -p
